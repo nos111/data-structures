3d64dab84db5f35da44e6bc65180ab262a6d95f2
Nour <nsaffour@gmail.com>
2018-01-18 21:37:44 +0100

    delete not working

--- binaryTree/tree.cpp
+++ binaryTree/tree.cpp
@@ -24,10 +24,11 @@ class BinaryTree {
                 if(temp->key == data) {
                     //Node with no childs
                     if(temp->left == nullptr && temp->right == nullptr) {
+                        //find wether it was the left or right child
                         if(temp->father->left != nullptr) {
                             if(temp->father->left->key == temp->key) temp->father->left = nullptr;
                             else  { temp->father->right = nullptr; }
-                        }
+                        } else { temp->father->right = nullptr; }
                         return;
                     }
                     //Node with one child
@@ -43,9 +44,40 @@ class BinaryTree {
                     }
 
                     //Node with two children
-                    while(temp != nullptr) {
-                        
+                    //find the node successor
+                    Node * successor = findSuccessor(temp);
+                    std::cout << "Key when returned from function is "<< successor->key << '\n';
+                    //if successor has right child connect it to the father of successor
+                    
+                    if(successor->right != nullptr) {
+                        //std::cout << "right hand key is " << successor->right->key << '\n';
+                        //std::cout << "successor key is "<< successor->key << '\n';
+                        successor->right->father = successor->father;
+                        if(successor->father->left != nullptr) {
+                            if(successor->father->left->key == successor->key) {
+                                successor->father->left = successor->right;
+                            }
+                        } else {
+                            successor->father->right = successor->right;
+                            }                            
+                    } else {
+                        if(successor->father->left != nullptr) {
+                            if(successor->father->left->key == successor->key) {
+                                successor->father->left = nullptr;
+                            }
+                        } else {
+                            successor->father->right = nullptr;
+                            std::cout << "got here ";
+                            }                            
+                        }
+                    //swap the successor to the node place
+                    successor->father = temp->father;
+                    if(temp->father->key > temp->key) {
+                        temp->father->left = successor;
+                    } else {
+                        temp->father->right = successor;
                     }
+                    return;
                 }
                 if (data > temp->key) {
                     temp = temp->right;
@@ -95,10 +127,54 @@ class BinaryTree {
                 }
             }
         }
-
-        void apply(void (*foo) (T)) {
-            foo(root->right->right->key);
+        Node * treeMaximum(Node * element) {
+            while(element->right != nullptr) {
+                element = element->right;
+            }
+            return element;
         }
+        Node * treeMinimum(Node * element) {
+            while(element->left != nullptr) {
+                element = element->left;
+            }
+            return element;
+        }
+        Node * findSuccessor(Node * element) {
+            if(element->right != nullptr) {
+                return treeMinimum(element->right);
+            }
+            Node * temp = element->father;
+            while(temp != nullptr && element == temp->right ) {
+                element = temp;
+                temp = temp->father;
+            }
+            return temp;
+        }
+        Node * findPredecessor(Node * element) {
+            if(element->left != nullptr) {
+                return treeMaximum(element->left);
+            }
+            Node * temp = element->parent;
+            while(temp != nullptr && element == temp->left ) {
+                element = temp;
+                temp = temp->father;
+            }
+            return temp;
+        }
+        void inorderTreeWalk(Node * x) {
+            if(x != nullptr) {
+                std::cout << x->key << "\n";
+                inorderTreeWalk(x->left);
+                inorderTreeWalk(x->right);
+            }
+        }
+        void apply(void (*foo) (T)) {
+            foo(root->right->right->left->key);
+        }
+        void printTree() {
+            inorderTreeWalk(root);
+        }
+
 
 };
 
@@ -116,10 +192,12 @@ int main() {
     bt->addElement(400);
     bt->addElement(50);
     bt->addElement(25);
+    bt->addElement(250);
     //int & a = bt->findElement(0);
     //a = 60;
     //std::cout << a << '\n';
-    bt->deleteElement(400);
+    bt->deleteElement(200);
     bt->apply(&print);
+    //bt->printTree();
     return 0;
 }
