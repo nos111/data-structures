42ab71a1737b537ce388ee20614fd5805c77e652
Nour <nsaffour@gmail.com>
2018-07-01 22:07:32 +0200

    added table class

--- hashTable/hash.cpp
+++ hashTable/hash.cpp
@@ -7,30 +7,55 @@ struct Element {
     key = newKey;
     data = newData;
   }
+  Element() { key = 0; data = 0;};
 };
 
 struct Slot {
   struct Element * element;
   bool deleted;
+  Slot() {
+    element = new Element();
+    deleted = false;
+  }
 };
 
-void insert(int key, int data) {
+class Table {
+  struct Slot * tableContent;
+  int tableSize;
+  int hash(int key, int trial, int tableSize);
+  public:
+    void insert(int key, int data);
+    void deleteElement(int key);
+    void find(int key);
+    Table() {
+      tableContent = new Slot[1024];
+      tableSize = 1024;
+    }
+    Table(int size) {
+      tableContent = new Slot[size];
+      tableSize = size;
+    }
+};
 
+void Table::insert(int key, int data) {
+  return;
 }
 
-void deleteElement(int key) {
-
+void Table::deleteElement(int key) {
+  return;
 }
 
-void find(int key) {
-
+void Table::find(int key) {
+  return;
 }
 
-int hash(int key, int trial, int tableSize) {
-
+int Table::hash(int key, int trial, int tableSize) {
+  return 0;
 }
 
 int main() {
+  Table * t = new Table();
+
   std::cout << " HELLO ME \n";
   return 0;
 }
