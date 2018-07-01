9fd70b4875d5c660f177972e7ea70f0377570fdb
Nour <nsaffour@gmail.com>
2018-07-01 21:37:56 +0200

    empty skeleton

--- hashTable/hash.cpp
+++ hashTable/hash.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 
+struct Element {
+  int key;
+  int data;
+  Element(int newKey, int newData) {
+    key = newKey;
+    data = newData;
+  }
+};
+
+struct Slot {
+  struct Element * element;
+  bool deleted;
+};
+
+void insert(int key, int data) {
+
+}
+
+void deleteElement(int key) {
+
+}
+
+void find(int key) {
+
+}
+
+int hash(int key, int trial, int tableSize) {
+
+}
+
 int main() {
   std::cout << " HELLO ME \n";
   return 0;
