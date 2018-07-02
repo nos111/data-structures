5394c56bd651cfd234dcd2ff5a07c5409c5b7b4f
Nour <nsaffour@gmail.com>
2018-07-02 20:10:14 +0200

    completed delete and fixed leaks

--- hashTable/hash.cpp
+++ hashTable/hash.cpp
@@ -19,6 +19,9 @@ struct Slot {
     deleted = false;
     empty = true;
   }
+  ~Slot() {
+    delete element;
+  }
 };
 
 class Table {
@@ -37,6 +40,14 @@ class Table {
       tableContent = new Slot [size];
       tableSize = size;
     }
+    ~Table() {
+      /*
+      for(int i = 0; i < tableSize; i++) {
+        struct Slot temp = tableContent[i];
+        delete(temp.element);
+      }*/
+      delete [] tableContent;
+    }
 };
 
 void Table::insert(int key, int data) {
@@ -51,31 +62,49 @@ void Table::insert(int key, int data) {
     std::cout << "the new index is " << index << " \n";
     temp = &tableContent[index];
   }
-  struct Element * newElement = new Element(key, data);
-  temp->element = newElement;
+  temp->element->key = key;
+  temp->element->data = data;
   temp->empty = false;
+  temp->deleted = false;
 }
 
 void Table::deleteElement(int key) {
-  return;
+  int trial = 1;
+  int index = hash(key, trial);
+  struct Slot * temp = &tableContent[index];
+  while(temp->empty == false || temp->deleted == true) {
+    if(temp->element->key == key) {
+      delete temp->element;
+      temp->element = new Element();
+      temp->deleted = true;
+      return;
+    }
+    trial += 1;
+    index = hash(key, trial);
+    temp = &tableContent[index];
+  }
 }
 
 int Table::find(int key) {
   int trial = 1;
   int index = hash(key, trial);
   struct Slot * slot = &tableContent[index];
-  while(slot->empty == false) {
+  while(slot->empty == false || slot->deleted == true) {
+    std::cout << "the slot empty is  " << slot->empty << " \n";
+    std::cout << "the slot deleted is  " << slot->deleted << " \n";
+    std::cout << "the slot key is  " << slot->element->key << " \n";
     if(slot->element->key == key) return slot->element->data;
     trial += 1;
     index = hash(key, trial);
+    slot = &tableContent[index];
   }
-  return NULL;
+  return 0;
 }
 
 //double hashing to avoid clustering
 int Table::hash(int key, int trial) {
   int hash1 = ((3 * key + 4) % 17) % tableSize;
-  int hash2 = ((11 * key + 13) % 17) % tableSize;
+  int hash2 = ((11 * key + 19) % 17) % tableSize;
   hash2 = (hash1 + trial * hash2) % tableSize;
   std::cout << "the new hash is " << hash2 << "\n";
   return hash2;
@@ -88,7 +117,10 @@ int main() {
   t->insert(20, 20);
   t->insert(500, 20);
   std::cout << " the value in key 500 is " << t->find(500) << " \n";
+  t->deleteElement(500);
+  std::cout << " the value in key 500 is " << t->find(500) << " \n";
   std::cout << " the value in key 10 is " << t->find(10) << " \n";
   std::cout << " HELLO ME \n";
+  delete t;
   return 0;
 }
